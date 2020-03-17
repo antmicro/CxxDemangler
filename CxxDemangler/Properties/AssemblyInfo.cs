d8f9cf8e1c3ac5da28b68e7530f52b86aaa18c0b
Mateusz Karlic <mkarlic@internships.antmicro.com>
2020-03-17 08:03:47 +0100

    [#14661] Change csproj to support .NET Framework

--- CxxDemangler/Properties/AssemblyInfo.cs
+++ CxxDemangler/Properties/AssemblyInfo.cs
@@ -1,34 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("CxxDemangler")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CxxDemangler")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b0b87f5-0c25-47dc-b507-709faa1020a2")]
-[assembly: InternalsVisibleTo("CxxDemangler.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003511cd1d7d6df45dbccb7001fd12c3c2e5171f59e80ebae225e7e4e0d80aece5d2fec51849b76a033e37049d2880cb5dbbe3f3e10e8dc0aeda8c54a2caa321e4f527e400a235bd6a8158926dce4a1e1cf11ebce2e1a804c086ed1068414561eb2dff5433fab52c45c33f790950996ea303cdd643325605d384cc3fc8ee066baf")]
+[assembly: AssemblyVersion("1.0.*")]
 
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.4")]
-[assembly: AssemblyFileVersion("0.2.4")]
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
